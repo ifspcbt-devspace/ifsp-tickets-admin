0a6cfc39e2de32459053a90abbca814e2880ad87
Leonardo da Silva <l.6042silva@gmail.com>
2025-09-09 18:07:08 -0300

    feat: configure static files settings for deployment

--- website/settings.py
+++ website/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
